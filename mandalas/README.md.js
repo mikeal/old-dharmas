a07d67f7062c1a5dcaf20eab5f74bb18dedaf13c
Mikeal Rogers <mikeal.rogers@gmail.com>
2023-10-08 09:44:00 -0700

    init mandalas

---
+++ mandalas/README.md.js
@@ -0,0 +1,37 @@
+const header = `# mandalas
+
+Mandalas are n-dimensional arrays that interconnect
+a set of teachings into a navigatabale structure that
+teaches the movement of such teachings by way of their
+arrangement.
+
+For example, we have teachings of the five tathagatas
+arranged here in the form of a table.
+
+| Akshobya | Ratnasambhava | Amitabha | Amoghasiddhi | Vairochana |
+| --- | --- | --- | --- | --- |
+| Immutable | Jewel-Producing | ♾️ Infinite Light | Unobstructed
+Success | Illuminator |
+| ➡️ East | ⬇️ South | ⬅️ West | ⬆️ North | 🫶 Center |
+| 🌅 Dawn | 🕛 Noon | 🌄 Sunset | 🌃 Midnight |  |
+| 💎 Vajra | 🟡 Jewel | 🪷 Lotus | ⚔️ Vajra Sword | ⚜️ Gold Dharmachakra |
+| 🐘 Elephant | 🐎 Horse | 🦚 Peacock | 🕊 Garuda | 🦁 Lion |
+| 👇 Earth-touching | 🤲 Supreme Giving | 🧘‍♂️ Meditation | 🪬Fearless (abhaya) | Turning the ☸️ Wheel |
+| hum | tram | hrih | ah ༁ | om ༀ |
+| 🗣 om vajra akshobhya hum | 🗣 om ratnasambhava tram | 🗣 om amiddeva hrih | 🗣 om amoghasiddhi ah hum | 🗣 om vairochana hum |
+|  🪞Mirror-like Wisdom  | 👯 Sameness Wisdom | 🧠 Discriminating Wisdom | 🙌 All-Accomplishing | 🌌 Dharmadhatu |
+| 🔄 Hatred | 🔄 Pride | 🔄 Greed | 🔄 Envy | 🔄 Ignorance |
+| Form | Feeling | Recognition | Volition | Consciousness |
+| 💦 Water | 🪨 Earth | 🔥 Fire | 🌬 Air | Space |
+| Relative Alaya | Manas | Mano | Sense | Absolute Alaya |
+| Joyous Land | Glorious Land | Happy Land | Land of Perfected Good Actions | All-Pervading Circle (Ghanavyuga) |
+| http://tibetanbuddhistencyclopedia.com/en/index.php?title=Buddha_lochana | http://www.tibetanbuddhistencyclopedia.com/en/index.php?title=Mamaki | https://www.wisdomlib.org/definition/pandaravasini | http://www.tibetanbuddhistencyclopedia.com/en/index.php?title=Samayatara | https://mandala-of-love.com/2019/06/15/akashadhateshvari-luminous-space/ |
+| Vajra Heruka | Ratna Heruka | Padma Heruka | Karma Heruka | Buddha Heruka |
+| Kshitigarbha, Maitreya | Akashagarbha, Samantabhadra | Avalokiteshvara, Manjushri | Vajrapani, Sarvanivaranavishkambhin |  |
+| Heart | Navel | Throat | Base | Crown |
+| Hell Realm | Human Realm | Hungry Ghost | Asuras (titans) | Gods |
+| 🪄 Destruction | 🪄 Enriching | 🪄 Attraction | 🪄 Pacifying all functions | 🪄 Pacifying |
+
+`
+
+console.log(header)
